e45cc7ca5401aab9f73480309b6be03a2484ec2f
andreysvyat <andreysvyatoy@gmail.com>
2024-12-21 19:36:34 +0300

    fix gateway

--- gateway/src/main/java/com/example/clinic/gatewayserver/GatewayServerApplication.java
+++ gateway/src/main/java/com/example/clinic/gatewayserver/GatewayServerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class, args);
